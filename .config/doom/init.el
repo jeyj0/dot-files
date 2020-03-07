63a6f07575e79421dd2a945ba5d6e467c8c31c15
Jannis Jorre <jannis@jorre.dev>
2020-03-07 13:45:15 +0100

    add docker support in doom

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -85,8 +85,8 @@
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
+       docker
        ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
