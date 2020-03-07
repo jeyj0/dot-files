22ddd2ab167a2f2fa8913d25604ff71375a32eec
Jannis Jorre <jeyj0.plus@gmail.com>
2020-03-07 17:34:42 +0100

    limit git log output in gco preview to 100 commits

--- .config/fish/functions/git_checkout_fzf.fish
+++ .config/fish/functions/git_checkout_fzf.fish
@@ -2,7 +2,7 @@ function gco -d "Fuzzy-find and checkout a branch"
   git branch --all \
     | grep -v '/HEAD\s' \
     | string trim \
-    | fzf --height 50% --preview 'glog-graph (echo {} | sed "s/^\* //")' --preview-window right:70% --ansi --tac --reverse \
+    | fzf --height 50% --preview 'glog-graph --max-count=100 (echo {} | sed "s/^\* //")' --preview-window right:70% --ansi --tac --reverse \
     | read -l result\
     ; and git checkout "$result"
 end
