f75eeaac10988fe886ccb97bc82e9160792b3c13
Jannis Jorre <jannis@jorre.dev>
2020-03-19 13:22:48 +0100

    remove outdated fzf-checkout in config.fish (see functions/git_checkoud_fzf)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,7 +24,3 @@ source $HOME/.cargo/env
 
 # use starship prompt
 starship init fish | source
-
-function fco -d "Fuzzy-find and checkout a branch"
-  git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
-end
