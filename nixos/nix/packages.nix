74030b3ad526ea55aadc1d8aa78fd246bd216974
Jannis Jorre <jannis@jorre.dev>
2020-11-02 15:39:03 +0100

    start my quest on using niv for pinning packages in NixOS

---
+++ nixos/nix/packages.nix
@@ -0,0 +1,12 @@
+{ sources ? import ./sources.nix }:
+let
+  overlay = _: pkgs: {
+    niv = (import sources.niv {}).niv;
+  };
+in
+  import sources.nixpkgs {
+    overlays = [
+      overlay
+    ];
+    config = {};
+  }
