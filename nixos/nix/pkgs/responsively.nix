be44e82efd0605ecb71e7186779f70aa4ac0750d
Jannis Jorre <jannis@jorre.dev>
2020-11-03 15:02:11 +0100

    update responsively to 0.15.0

--- nixos/nix/pkgs/responsively.nix
+++ nixos/nix/pkgs/responsively.nix
@@ -5,8 +5,8 @@ let
 
   name = "responsively";
   src = fetchurl {
-    url = "https://github.com/manojVivek/responsively-app/releases/download/v0.4.0/ResponsivelyApp-0.4.0.AppImage";
-    sha256 = "0n17hipkys3zhihpspll2bx40338fikq428i50bp5zip9gd1i1r7";
+    url = "https://github.com/responsively-org/responsively-app/releases/download/v0.15.0/ResponsivelyApp-0.15.0.AppImage";
+    sha256 = "101mg73qalkhbx47xk8swk067x9ll7ff63icgjmmw06xv3d26rgw";
   };
 in
 wrapType2 {
@@ -19,6 +19,6 @@ Encoding=UTF-8
 Type=Application
 Exec=responsively
 Name=Responsively App
-Version=0.4.0" > $out/share/applications/responsively.desktop
+Version=0.15.0" > $out/share/applications/responsively.desktop
   '';
 }
