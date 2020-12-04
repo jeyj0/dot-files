0ad1066be17537d3c3bcee2bb5c476d1f83adc7f
Jannis Jorre <jannis@jorre.dev>
2020-12-04 15:15:41 +0100

    add NUR - nix user repository to packages

--- nixos/nix/packages.nix
+++ nixos/nix/packages.nix
@@ -3,6 +3,13 @@ let
   overlay = _: pkgs: {
     niv = (import sources.niv {}).niv;
 
+    nur = import (builtins.fetchTarball {
+      url = "https://github.com/nix-community/NUR/archive/cc3d8311e40485e881c56b159aa3c824b877a0e9.tar.gz";
+      sha256 = "04jfh0hbcwxy6rdk054gnmfc0ig6sr0s5fhkwwg90gn8hgmgln8l";
+    }) {
+      inherit pkgs;
+    };
+
     responsively = pkgs.callPackage (import ./pkgs/responsively.nix) {};
     nnn = pkgs.callPackage (import ./pkgs/nnn.nix) {};
     battery-status = import ../../scripts/battery-status;
