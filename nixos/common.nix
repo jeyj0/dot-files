1b6333ab046266dd3c22dd3122b9925952fc6b16
Jannis Jorre <jannis@jorre.dev>
2020-12-04 15:19:31 +0100

    remove i3 from configuration

--- nixos/common.nix
+++ nixos/common.nix
@@ -181,15 +181,6 @@ in
     };
 
     windowManager = {
-      i3 = {
-        enable = true;
-        package = pkgs.i3-gaps;
-        extraPackages = with pkgs; [
-          dmenu
-          rofi
-          polybarFull
-        ];
-      };
       xmonad = {
         enable = true;
         enableContribAndExtras = true;
