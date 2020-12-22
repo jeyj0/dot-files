ea86774a37cf3ee6667cbf98652def2d79c9f214
Jannis Jorre <jannis@jorre.dev>
2020-12-22 21:20:25 +0100

    manage config.fish through home-manager

--- .config/home-manager/parts/common.nix
+++ .config/home-manager/parts/common.nix
@@ -1,6 +1,14 @@
 { pkgs }:
 {
   imports = [
+    (import ./fish {
+      extraConfigPre = ''
+        set -x NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
+        set -x NIX_SSL_CERT_FILE "$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt"
+        set -x NIX_PATH /nix $HOME/.nix-defexpr/channels
+        set -x PATH $HOME/.nix-profile/bin $PATH
+      '';
+    })
     (import ./cli-tools { pkgs = pkgs; })
     (import ./neovim { pkgs = pkgs; })
     (import ./emacs { pkgs = pkgs; })
---
+++ .config/home-manager/parts/fish/default.nix
@@ -0,0 +1,43 @@
+{ extraConfigPre ? "", extraConfigPost ? "" }:
+{
+  programs.fish = {
+    enable = true;
+    loginShellInit = extraConfigPre + ''
+      set FISH_PATH $HOME/.config/fish
+
+      # install fundle if not present (package manager for fish)
+      if not functions -q fundle
+        eval (curl -sfL https://git.io/fundle-install)
+      end
+
+      fundle plugin 'edc/bass'
+      fundle plugin 'jeyj0/fzf-fish-integration'
+
+      fundle init
+
+      source $FISH_PATH/paths.fish
+      source $FISH_PATH/aliases.fish
+
+      # do not show a welcome message
+      set fish_greeting
+
+      # use starship prompt
+      starship init fish | source
+
+      if test -d $NVM_DIR
+        source $FISH_PATH/nvm.fish
+      end
+
+      # load rust
+      if test -e $HOME/.cargo/env
+        source $HOME/.cargo/env
+      end
+
+      # add direnv hook
+      eval (direnv hook fish)
+
+      # source nnn config
+      source $FISH_PATH/nnn.fish
+    '' + extraConfigPost;
+  };
+}
