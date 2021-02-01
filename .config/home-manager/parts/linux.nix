bdbbde0d6d0185595e3666ca7961d68e64034baf
Jannis Jorre <jannis@jorre.dev>
2021-02-01 12:55:33 +0100

    move non-macos supported browsers to linux config in home-manager

--- .config/home-manager/parts/linux.nix
+++ .config/home-manager/parts/linux.nix
@@ -21,5 +21,9 @@
     lxc
 
     wonderdraft
+
+    chromium
+    qutebrowser
+    responsively
   ];
 }
--- .config/home-manager/parts/webdev/default.nix
+++ .config/home-manager/parts/webdev/default.nix
@@ -1,9 +1,6 @@
 { pkgs }:
 {
   home.packages = with pkgs; [
-    chromium
-    qutebrowser
-    responsively
     # postman
   ];
 }
