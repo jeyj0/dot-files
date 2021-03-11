0dff69541b53206c2f700c99b27c33434dc9ee09
Jannis Jorre <jannis@jorre.dev>
2021-03-11 13:09:36 +0100

    package lycheeslicer

---
+++ nixos/nix/pkgs/lychee-slicer/derivation.nix
@@ -0,0 +1,22 @@
+{ stdenv, appimageTools
+, gsettings-desktop-schemas
+, gtk3
+}:
+appimageTools.wrapType2 {
+  name = "lycheeslicer";
+
+  src = ./LycheeSlicer-3.3.1.AppImage;
+
+  profile = ''
+    export LC_ALL=C.UTF-8
+    export XDG_DATA_DIRS="${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS"
+  '';
+
+  extraPkgs = pkgs: with pkgs; [
+    # fixes "unexpected error"
+    gsettings_desktop_schemas glib gtk3
+
+    # needed for icons
+    gnome3.defaultIconTheme
+  ];
+}
