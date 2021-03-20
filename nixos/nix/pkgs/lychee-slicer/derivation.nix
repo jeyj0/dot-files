ad13fd17b7bcc01822313879de59084f43e9ca6e
Jannis Jorre <jannis@jorre.dev>
2021-03-20 14:54:16 +0100

    better package lychee (support internet access)

--- nixos/nix/pkgs/lychee-slicer/derivation.nix
+++ nixos/nix/pkgs/lychee-slicer/derivation.nix
@@ -12,11 +12,11 @@ appimageTools.wrapType2 {
     export XDG_DATA_DIRS="${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS"
   '';
 
-  extraPkgs = pkgs: with pkgs; [
+  extraPkgs = pkgs: (appimageTools.defaultFhsEnvArgs.multiPkgs pkgs) ++ (with pkgs; [
     # fixes "unexpected error"
     gsettings_desktop_schemas glib gtk3
 
     # needed for icons
     gnome3.defaultIconTheme
-  ];
+  ]);
 }
