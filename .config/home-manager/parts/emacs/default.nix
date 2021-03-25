7c8db2aca15ec99d6d57d795f09d50869ddc571e
Jannis Jorre <jannis@jorre.dev>
2021-03-25 14:40:13 +0100

    download org-transclusion via home-manager

--- .config/home-manager/parts/emacs/default.nix
+++ .config/home-manager/parts/emacs/default.nix
@@ -1,6 +1,12 @@
 { pkgs }:
 let
   emacsConfig = ./emacs.el;
+  orgTransclusion = pkgs.fetchFromGitHub {
+    owner = "nobiot";
+    repo = "org-transclusion";
+    rev = "c1b20436911f364c9477a17905af2881c765da14";
+    sha256 = "0plb0wj461qb4crfbk6lsl45ixj0fz6xddcv46n5fs829d901hds";
+  };
 in
 {
   programs.emacs = {
@@ -24,8 +30,14 @@ in
     plantuml
   ];
 
-  home.file.emacsConfig = {
-    source = emacsConfig;
-    target = ".emacs";
+  home.file = {
+    emacsConfig = {
+      source = emacsConfig;
+      target = ".emacs";
+    };
+    orgTransclusion = {
+      source = orgTransclusion;
+      target = ".emacs.d/org-transclusion/";
+    };
   };
 }
