e0f16e9bea95747611925bdc2543a239671bc393
Jannis Jorre <jannis@jorre.dev>
2022-05-22 09:55:43 +0200

    update gitignore; update nvim home screen

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -15,14 +15,14 @@ dashboard.section.header.val = {
 
 -- Set menu
 dashboard.section.buttons.val = {
-    dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-    dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
-    dashboard.button("g", "  > Find Text   ", ":Telescope live_grep<CR>"),
-    dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
-    -- dashboard.button("l", "  > List session",
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+    dashboard.button("g", "  Find Text   ", ":Telescope live_grep<CR>"),
+    dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+    -- dashboard.button("l", "  List session",
     --     ":SessionManager load_session<CR>"),
-    dashboard.button("u", "  > Update plugins", ":UpdateAll<CR>"),
-    dashboard.button("q", "  > Quit NVIM", ":qa<CR>")
+    dashboard.button("u", "  Update plugins", ":UpdateAll<CR>"),
+    dashboard.button("q", "  Quit NVIM", ":qa<CR>")
 }
 
 -- Send config to alpha
