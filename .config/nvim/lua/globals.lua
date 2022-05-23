02a47e163676b039ddadb4e3c9e65dbc2678b4d7
Jannis Jorre <jannis@jorre.dev>
2022-05-23 09:07:41 +0200

    nvim: use smart case search

--- .config/nvim/lua/globals.lua
+++ .config/nvim/lua/globals.lua
@@ -1,1 +1,8 @@
+-- enable useful mouse support
 vim.opt.mouse = 'a'
+
+-- make search case insensitive by default
+vim.opt.ignorecase = true
+-- make search case sensitive when capital letters are involved
+vim.opt.smartcase = true
+
