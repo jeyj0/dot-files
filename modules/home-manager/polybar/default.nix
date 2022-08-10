663c5ba8f6d33a7be478d988d13ad396c8e277f9
Jannis Jorre <jannis@jorre.dev>
2022-08-10 13:26:31 +0200

    base polybar config

--- modules/home-manager/polybar/default.nix
+++ modules/home-manager/polybar/default.nix
@@ -12,11 +12,14 @@ with lib;
       settings = {
         "bar/desktop-left" = {
           monitor = "DP-2";
+          height = 32;
           modules = {
             # left = "cpu ram";
             left = "cpu";
           };
           font = ["Hack Nerd Font"];
+          background = "#1d2021";
+          foreground = "#fbf1c7";
         };
         "module/cpu" = {
           type = "internal/cpu";
