968b292c7bc2a82e7fdfba4212bf9789dc0c3d2b
Jannis Jorre <jannis@jorre.dev>
2022-08-10 18:52:40 +0200

    add system info to polybar

--- modules/home-manager/polybar/default.nix
+++ modules/home-manager/polybar/default.nix
@@ -8,26 +8,71 @@ with lib;
   config = mkIf config.jeyj0.polybar.enable {
     services.polybar = {
       enable = true;
-      script = "polybar desktop-left &";
+      package = pkgs.unstable.polybar;
+      script = "polybar desktop-left & polybar desktop-right &";
       settings = {
-        "bar/desktop-left" = {
-          monitor = "DP-2";
+        "bar/base" = {
           height = 32;
           modules = {
-            # left = "cpu ram";
-            left = "xmonad cpu";
+            left = "start xmonad";
+            right = "cpu ram disk date";
           };
           font = ["Hack Nerd Font"];
           background = "#1d2021";
           foreground = "#fbf1c7";
+          module.margin = 1;
+        };
+        "bar/desktop-left" = {
+          "inherit" = "bar/base";
+          monitor = "DP-2";
+        };
+        "bar/desktop-right" = {
+          "inherit" = "bar/base";
+          monitor = "DP-4";
         };
         "module/cpu" = {
           type = "internal/cpu";
+          # format = "<label> <ramp-coreload>";
+          format = "<label>";
+          format-foreground = "#ecbe7b";
+          label = " %percentage%%";
+          label-warn = " %percentage%%";
+          ramp-coreload = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+        };
+        "module/ram" = {
+          type = "internal/memory";
+          format-foreground = "#ff6c6b";
+          format = " <label>";
+          label = "%used%";
+        };
+        "module/disk" = {
+          type = "internal/fs";
+          mount = ["/"];
+          format-mounted = "<label-mounted>";
+          format-mounted-foreground = "#51afef";
+          label-mounted = " %free%";
+        };
+        "module/date" = {
+          type = "internal/date";
+          format = " <label> ";
+          format-foreground = "#46d9ff";
+          label = "%date% %time%";
+          date = "%a, %d %b";
+          time = "%H:%M";
+          date-alt = "%Y-%m-%d%";
+          time-alt = "%H:%M";
         };
         "module/xmonad" = {
           type = "custom/script";
           exec = "${pkgs.xmonad-log}/bin/xmonad-log";
           tail = true;
+        };
+        "module/start" = {
+          type = "custom/text";
+          content = " NixOS";
+          content-padding = 1;
+          content-background = "#8ec07c";
+          content-foreground = "#1d2021";
         };
       };
     };
