52176eee5c92485d0f2f003b34b57af2e8ed9826
Jannis Jorre <jannis@jorre.dev>
2022-08-12 21:20:27 +0200

    temporarily disable helix config (not supported in current home-manager version)

--- modules/home-manager/helix/default.nix
+++ modules/home-manager/helix/default.nix
@@ -10,21 +10,21 @@ with lib;
   };
 
   config = mkIf config.jeyj0.helix.enable {
-    programs.helix = {
-      enable = true;
-      package = pkgs.unstable.helix;
-      settings = {
-        theme = "gruvbox_contrast";
-        keys.normal = {
-          space = {
-            space = "file_picker";
-            f = {
-              s = ":write";
-            };
-          };
-        };
-      };
-    };
+    # programs.helix = {
+    #   enable = true;
+    #   package = pkgs.unstable.helix;
+    #   settings = {
+    #     theme = "gruvbox_contrast";
+    #     keys.normal = {
+    #       space = {
+    #         space = "file_picker";
+    #         f = {
+    #           s = ":write";
+    #         };
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
 
