3044127393dec2935a7bc4fcf5dc7ac22663a930
Jannis Jorre <jannis@jorre.dev>
2023-01-04 19:04:03 +0100

    use default obsidian desktop setup

--- modules/home-manager/obsidian/default.nix
+++ modules/home-manager/obsidian/default.nix
@@ -11,9 +11,5 @@ with lib;
 
   config = mkIf config.jeyj0.obsidian.enable {
     home.packages = with pkgs.unstable; [ obsidian ];
-    home.file.obsidianDesktopEntry = {
-      source = ./obsidian.desktop;
-      target = ".local/share/applications/obsidian.desktop";
-    };
   };
 }
