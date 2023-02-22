31458eb2ae3a59a6a7d846527da582b47c7fc0fb
Jannis Jorre <jannis@jorre.dev>
2023-02-22 17:09:54 +0100

    switch to tokyonight for anything terminal

--- modules/home-manager/alacritty/default.nix
+++ modules/home-manager/alacritty/default.nix
@@ -26,22 +26,44 @@ with lib;
           bold_italic = { family = "Hack Nerd Font"; style = "Bold Italic"; };
         };
         colors = {
+          # TokyoNight Alacritty Colors
+          # Default colors
           primary = {
-            background = "#1c1c1c";
-            foreground = "#ebdbb2";
+            background = "0x222436";
+            foreground = "0xc8d3f5";
           };
+
+          # Normal colors
           normal = {
-            black = "#282828";
-            red = "#cc241d";
-            green = "#98971a";
-            yellow = "#d79921";
-            blue = "#458588";
-            magenta = "#b16286";
-            cyan = "#689d6a";
-            white = "#ebdbb2";
+            black =   "0x1b1d2b";
+            red =     "0xff757f";
+            green =   "0xc3e88d";
+            yellow =  "0xffc777";
+            blue =    "0x82aaff";
+            magenta = "0xc099ff";
+            cyan =    "0x86e1fc";
+            white =   "0x828bb8";
           };
+
+          # Bright colors
+          bright = {
+            black =   "0x444a73";
+            red =     "0xff757f";
+            green =   "0xc3e88d";
+            yellow =  "0xffc777";
+            blue =    "0x82aaff";
+            magenta = "0xc099ff";
+            cyan =    "0x86e1fc";
+            white =   "0xc8d3f5";
+          };
+
+          indexed_colors = [
+            { index = 16; color = "0xff966c"; }
+            { index = 17; color = "0xc53b53"; }
+          ];
         };
       };
     };
   };
 }
+
