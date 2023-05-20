6d7c569a8f10b9ccce1002b65f4620ddd8754f47
Jannis Jorre <jannis@jorre.dev>
2023-05-20 10:11:31 +0200

    add missing nix config

--- modules/home-manager/nix/default.nix
+++ modules/home-manager/nix/default.nix
@@ -14,6 +14,8 @@ with lib;
       package = pkgs.unstable.nix;
       settings = {
         experimental-features = "nix-command flakes";
+        substituters = "https://cache.nixos.org https://cache.nixos.org/ https://digitallyinduced.cachix.org https://nix-community.cachix.org";
+        trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= digitallyinduced.cachix.org-1:y+wQvrnxQ+PdEsCt91rmvv39qRCYzEgGQaldK26hCKE= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
       };
     };
   };
