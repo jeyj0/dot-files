1eee6278f46527fdcb7b37bb6ba08ce16dd56a97
Jannis Jorre <jannis@jorre.dev>
2023-06-05 16:05:02 +0200

    add more dconf settings to home-manager gnome config

--- modules/home-manager/gnome/default.nix
+++ modules/home-manager/gnome/default.nix
@@ -13,6 +13,8 @@ with lib;
     jeyj0.xfconf.enable = true;
     # themeing
     dconf.settings = {
+      # use `dconf watch /` and change what you want to save to figure out what to set here
+
       #"/org/gnome/desktop/wm/keybindings/switch-applications" = "";#[];
       #"/org/gnome/desktop/wm/keybindings/switch-windows" = "<Alt>Tab";#["<Alt>Tab"];
       #"/org/gnome/desktop/wm/keybindings/switch-windows-backward" = "<Shift><Alt>Tab";#["<Shift><Alt>Tab"];
@@ -21,6 +23,24 @@ with lib;
         switch-windows = ["<Alt>Tab"];
         switch-windows-backward = ["<Shift><Alt>Tab"];
       };
+
+      # enable right-alt/alt-gr as compose key
+      "org/gnome/desktop/input-sources".xkb-options = ["terminate:ctrl_alt_bksp" "compose:ralt"];
+
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+      "org/gnome/desktop/notifications".show-in-lock-screen = false;
+
+      # turn off automatic screen brightness
+      "org/gnome/settings-daeom/plugins/power".ambient-enabled = false;
+
+      "org/gnome/desktop/peripherals/touchpad" = {
+        speed = 0.33333333333333326;
+        tap-to-click = true;
+      };
+
+      # alt-tab only switches windows on current workspace
+      "org/gnome/shell/app-switcher".current-workspace-only = true;
 
       "org/gnome/shell" = {
         disable-user-extensions = false;
