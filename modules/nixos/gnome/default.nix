45da04a67e6d2eda6fafbcda8c6d451f34532523
Jannis Jorre <jannis@jorre.dev>
2023-11-28 08:33:21 +0100

    nixos (framework): add commented line to show how to disable wayland in gnome

--- modules/nixos/gnome/default.nix
+++ modules/nixos/gnome/default.nix
@@ -12,6 +12,7 @@ with lib;
   config = mkIf config.jeyj0.gnome.enable {
     # Enable the GNOME Desktop Environment.
     services.xserver.displayManager.gdm.enable = true;
+    # services.xserver.displayManager.gdm.wayland = false;
     services.xserver.desktopManager.gnome.enable = true;
   };
 }
