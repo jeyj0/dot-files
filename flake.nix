8751817c80819fcd3cfda525747f1aae5b22de95
Jannis Jorre <jannis@jorre.dev>
2025-09-11 21:36:48 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."jeyj0" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,122 @@
+{ config, lib, pkgs, ... }:
+{
+  home.stateVersion = "24.05";
+  home.username = "jeyj0";
+  home.homeDirectory = "/home/jeyj0";
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      extra-experimental-features = ["nix-command" "flakes"];
+    };
+  };
+
+  programs.home-manager.enable = true;
+  programs.eza.enable = true;
+  programs.bat.enable = true;
+  programs.zoxide = {
+    enable = true;
+    options = ["--cmd cd"];
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      cp = "cp -i";
+      mv = "mv -i";
+      tree = "eza --tree";
+
+      gs = "git status";
+      gss = "git status -s";
+      gc = "git commit";
+      ga = "git add";
+      gd = "git diff";
+      gdc = "git diff --cached";
+    };
+  };
+
+  programs.zellij = {
+    enable = true;
+    enableFishIntegration = true;
+    exitShellOnExit = true;
+    settings = {
+      show_startup_tips = false;
+      simplified_ui = true;
+      pane_frames = false;
+      ui = {
+        pane_frames = {
+          hide_session_name = true;
+        };
+      };
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+      directory = {
+        truncation_length = 1;
+      };
+      nix_shell.disabled = true;
+      battery.display = [{
+        threshold = 100;
+      }];
+    };
+  };
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "base16_terminal";
+      editor = {
+        bufferline = "multiple";
+        auto-format = true;
+        auto-completion = true;
+        completion-trigger-len = 1;
+        gutters = ["diff" "diagnostics" "spacer" "line-numbers" "spacer"];
+        idle-timeout = 0;
+        color-modes = true;
+        indent-guides.render = true;
+
+        lsp.goto-reference-include-declaration = false;
+      };
+      keys = {
+        select = {
+          "}" = "goto_next_paragraph";
+          "{" = "goto_prev_paragraph";
+        };
+        normal = {
+          "}" = "goto_next_paragraph";
+          "{" = "goto_prev_paragraph";
+          space = {
+            space = "file_picker";
+            "." = "code_action";
+            c = "toggle_comments";
+            f = {
+              s = ":write";
+            };
+            b = {
+              b = "buffer_picker";
+              n = ":buffer-next";
+              p = ":buffer-previous";
+              d = ":buffer-close";
+              D = ":buffer-close!";
+              O = ":buffer-close-others";
+            };
+          };
+        };
+        insert = {
+          "C-space" = "completion";
+        };
+      };
+    };
+  };
+}
